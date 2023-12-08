5ea9590a06f6b181129f320d72e52416e74871db
SamirNeu17 <sawmeer.neupane@gmail.com>
2023-12-08 08:07:42 +0545

    login bakcend

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ import users.views as user_views
 
 urlpatterns = [
     path("",user_views.index, name="index"),
-    path("login",user_views.login,name="login"),
+    path("login",user_views.user_login,name="login"),
     path("register",user_views.register,name="register"),
     path("profile",user_views.user_profile,name="profile"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,16 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+import users.forms as forms
+from users.models import User, Profile
+from django.contrib import messages
+from django.contrib.auth import authenticate, login,logout
 
 #Create your views here.
 
 def index(request):
     return render(request,"index.html")
 
-def login(request):
+def user_login(request):
+    if request.method =="POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        check_user = User.objects.filter(email=email)
+        if check_user.exists() is False:
+            error_message="Email  does not exist"
+            messages.error(request,error_message)
+            return redirect ("login")
+    username= check_user[0].username
+    valid_user = authenticate(username=username,password=password)
+    if valid_user:
+        login(request,valid_user)
+        return redirect("profile")
+    else:
+        error_message ="Invalid email or password"
+        message.error(request,error_message)
+        return redirect("login")
     return render(request,"login.html")
 
 def register(request):
+    
     return render (request,"register.html")
+
+
+def register(request):
+    register_form = forms.RegisterForm()
+    context= {"form": register_form}
+    return render (request,"register.html",context)
+
+
 
 def user_profile(request):
     return render (request,"profile.html")
